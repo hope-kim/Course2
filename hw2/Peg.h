3e37f45315b36be05350ffa32d5069f1fb656a94
Hope Kim <hopekim@usc.edu>
2018-08-17 13:22:23 -0700

    Update and rename Peg.h to hw2/Peg.h

--- hw2/Peg.h
+++ hw2/Peg.h
@@ -1,5 +1,5 @@
 // ITP 365 Fall 2017
-// HW3 Towers of Hannoi
+// HW2 Towers of Hannoi
 // Name: Hope Kim
 // Email: hopekim@usc.edu
 // Platform: Mac
@@ -24,6 +24,3 @@ public:
     void addDisk(Disk& input);
     Disk removeDisk();
 };
-
-
-
